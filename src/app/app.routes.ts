ccdb8f042160be1e4a806e02c1168d7b48097a5b
M1roel <ppfautsch@yahoo.de>
2025-04-28 15:18:45 +0200

    mixins.scss, varaibles.scss und styles.scss eingerichtet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DashbordComponent } from './features/dashbord/dashbord.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashbordComponent}
+];
