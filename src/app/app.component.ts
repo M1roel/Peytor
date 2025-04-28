6ea7077b13f35800f08af9fdd08d3252b6bdcfb0
M1roel <ppfautsch@yahoo.de>
2025-04-28 15:44:45 +0200

    style: styling für header, main-section und footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from './shared/footer/footer.component';
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss', '../styles.scss']
 })
 export class AppComponent {
   title = 'Peytor';
