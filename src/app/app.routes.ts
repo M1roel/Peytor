adb6d137bd0d0193fafca74b159c2aa3797ac855
M1roel <ppfautsch@yahoo.de>
2025-05-02 20:17:00 +0200

    feat: Layout-Components für MainLayout und AppLayout hinzugefügt und Routen angepasst

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+import { CustomersComponent } from './features/customers/customers.component'; 
 import { DashbordComponent } from './features/dashbord/dashbord.component';
 import { HomeComponent } from './features/landing/home/home.component';
 import { PricingComponent } from './features/landing/pricing/pricing.component';
@@ -6,14 +9,32 @@ import { AboutComponent } from './features/landing/about/about.component';
 import { ContactComponent } from './features/landing/contact/contact.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { LoginComponent } from './features/auth/login/login.component';   
+import { InvoicesComponent } from './features/invoices/invoices.component'; 
+import { SettingsComponent } from './features/settings/settings.component'; 
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'pricing', component: PricingComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'register', component: RegisterComponent},
-  { path: 'dashboard', component: DashbordComponent},
-  { path: 'login', component: LoginComponent}
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'pricing', component: PricingComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
+    ]
+  },
+  {
+    path: 'app',
+    component: AppLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashbordComponent },
+      { path: 'customers', component: CustomersComponent },
+      { path: 'invoices', component: InvoicesComponent },
+      { path: 'settings', component: SettingsComponent },
+    ]
+  }
 ];
