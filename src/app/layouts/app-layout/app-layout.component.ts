6e62002bffb02c0d8bccef50e59cd70382cdfc66
M1roel <ppfautsch@yahoo.de>
2025-05-03 22:09:19 +0200

    feat: Seitenleiste hinzugefügt

--- src/app/layouts/app-layout/app-layout.component.ts
+++ src/app/layouts/app-layout/app-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  isOpen: boolean = true;
 
+  toggleSidebar(): void {
+    this.isOpen = !this.isOpen;
+  }
 }
