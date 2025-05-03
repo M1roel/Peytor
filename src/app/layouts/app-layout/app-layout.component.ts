94b65b5297a0c01c6ca3ecd271d0f8a62bb62015
M1roel <ppfautsch@yahoo.de>
2025-05-03 22:28:41 +0200

    feat: auf- und zuklappbare Seitenleiste

--- src/app/layouts/app-layout/app-layout.component.ts
+++ src/app/layouts/app-layout/app-layout.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, CommonModule],
   templateUrl: './app-layout.component.html',
-  styleUrl: './app-layout.component.scss'
+  styleUrl: './app-layout.component.scss',
 })
 export class AppLayoutComponent {
+  isSidebarOpen: boolean = true;
 
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
