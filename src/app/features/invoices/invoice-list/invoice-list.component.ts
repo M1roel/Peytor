33bb4c4d1c40e9a7c7c94f5e8b5368f866fba585
M1roel <ppfautsch@yahoo.de>
2025-05-04 21:47:53 +0200

    feat: invoice-list component hinzugefügt

--- src/app/features/invoices/invoice-list/invoice-list.component.ts
+++ src/app/features/invoices/invoice-list/invoice-list.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule, CurrencyPipe, DatePipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-invoice-list',
-  imports: [],
+  imports: [NgClass, CommonModule, CurrencyPipe, DatePipe],
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.scss'
 })
 export class InvoiceListComponent {
 
+  invoices = [
+    { id: 1, number: 'INV-001', customer: 'Peter GmbH', date: new Date(), status: 'bezahlt', amount: 199.99 },
+    { id: 2, number: 'INV-002', customer: 'Müller AG', date: new Date(), status: 'offen', amount: 349.50 },
+  ];
+
+  view(id: number) {
+    // später: this.router.navigate(['detail', id]);
+    console.log('Ansehen', id);
+  }
+
+  edit(id: number) {
+    console.log('Bearbeiten', id);
+  }
+
+  delete(id: number) {
+    console.log('Löschen', id);
+  }
 }
+
