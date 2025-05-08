48415ef2fa0d9c782061cc9379dcf022452b9844
M1roel <ppfautsch@yahoo.de>
2025-05-08 12:38:19 +0000

    feat: update .gitignore to ignore env files and environments.ts; refactor app.config to use environment variables for Firebase initialization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,15 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { environment } from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "peytor-4dedd", appId: "1:970206228738:web:c7291945d01faea65cd21b", storageBucket: "peytor-4dedd.firebasestorage.app", apiKey: "AIzaSyC8nqvDUKjHJpobkxmc007Lyl8eKjObj88", authDomain: "peytor-4dedd.firebaseapp.com", messagingSenderId: "970206228738", measurementId: "G-SMDLK1BHNY" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ]
 };
