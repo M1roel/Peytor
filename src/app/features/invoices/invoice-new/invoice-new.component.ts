9c72b467dcb8209cb78f395fbd0926e86f0089a7
M1roel <ppfautsch@yahoo.de>
2025-05-09 12:19:44 +0000

    feat: add InvoiceNewComponent with template and unit tests

---
+++ src/app/features/invoices/invoice-new/invoice-new.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-new',
+  imports: [],
+  templateUrl: './invoice-new.component.html',
+  styleUrl: './invoice-new.component.scss'
+})
+export class InvoiceNewComponent {
+
+}
