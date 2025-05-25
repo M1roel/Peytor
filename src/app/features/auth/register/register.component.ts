b4714da9689d3dd8ccab6c61275cc1407dfacd67
M1roel <ppfautsch@yahoo.de>
2025-05-25 15:35:39 +0200

    style: enhance registration and login forms with error handling and UI improvements

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -4,27 +4,57 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../core/services/auth.service';
 import { User } from '@angular/fire/auth';
+import { ErrorMessagesService } from '../../../core/services/error-messages.service';
 
 @Component({
   selector: 'app-register',
   imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-
 export class RegisterComponent {
-name: string = '';
-email: string = '';
-password: string = '';
-confirmPassword: string = '';
-user: User | null = null;
-disabled: boolean = true;
-termsAccepted: boolean = false;
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  user: User | null = null;
+  disabled: boolean = true;
+  termsAccepted: boolean = false;
+  errorMessage: string = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private errorMessageService: ErrorMessagesService
+  ) {}
 
-  async onSubmit(){
-    const userCrentials = this.authService.createUser(this.email, this.password);
+  async onSubmit() {
+    await this.validateForm();
+    const userCrentials = this.authService.createUser(
+      this.email,
+      this.password
+    );
     console.log((await userCrentials).user);
-  } 
+  }
+
+  validateForm() {
+    if (!this.name)
+      this.errorMessage =
+        this.errorMessageService.getFieldRequiredMessage('einen Namen');
+    if (!this.email) {
+      this.errorMessage =
+        this.errorMessageService.getFieldRequiredMessage('E-Mail-Adresse');
+    } else if (!this.isValidEmail(this.email)) {
+      this.errorMessage =
+        this.errorMessageService.getInvalidEmailFormatMessage();
+    }
+    if (!this.password) {
+      this.errorMessage =
+        this.errorMessageService.getFieldRequiredMessage('ein Passwort');
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
 }
