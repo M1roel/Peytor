3cc16aa91ca22ed7ba4fac21196259213fb2c815
M1roel <ppfautsch@yahoo.de>
2025-05-27 20:40:58 +0200

    style: refactor FireService to include userData property and AuthService dependency

--- src/app/core/services/fire.service.ts
+++ src/app/core/services/fire.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { addDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FireService {
 
-  constructor() { }
+  public userData: any = {};
+
+  constructor(authService: AuthService) { }
+
+  storeUser(name: string) {
+  }
 }
