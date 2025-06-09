a1cad7b001621c087119f29e0b03df196d4e271b
M1roel <ppfautsch@yahoo.de>
2025-06-09 19:59:31 +0200

    style: replace logout link with button in sidebar and update component for logout functionality

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,9 +10,20 @@ import { RouterModule } from '@angular/router';
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  constructor(private router: Router, private authService: AuthService) {}
+
   isOpen: boolean = true;
 
   toggleSidebar(): void {
     this.isOpen = !this.isOpen;
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await this.authService.logoutUser();
+      this.router.navigate(['/login']); // Zielseite anpassen
+    } catch (error) {
+      console.error('Fehler beim Logout:', error);
+    }
   }
 }
