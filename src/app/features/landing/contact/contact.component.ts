919e6d6ab4db804bad97182aa25abe4ccc88409c
M1roel <ppfautsch@yahoo.de>
2025-06-12 17:48:37 +0200

    style: simplify form submission logic and remove mail test condition

--- src/app/features/landing/contact/contact.component.ts
+++ src/app/features/landing/contact/contact.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ErrorMessagesService } from '../../../core/services/error-messages.service'; 
+import { ErrorMessagesService } from '../../../core/services/error-messages.service';
 
 @Component({
   selector: 'app-contact',
@@ -12,7 +12,6 @@ import { ErrorMessagesService } from '../../../core/services/error-messages.serv
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-
   private http = inject(HttpClient);
 
   contactData = {
@@ -21,7 +20,6 @@ export class ContactComponent {
     message: '',
   };
 
-  mailTest = false;
   errorMessage: string = '';
   showSuccess: boolean = false;
 
@@ -39,56 +37,48 @@ export class ContactComponent {
   constructor(private errorService: ErrorMessagesService) {}
 
   async onSubmit(ngForm: NgForm) {
-    if(!this.checkEntries()) return;
+    if (!this.checkEntries()) return;
     if (ngForm.submitted) {
-      if (!this.mailTest) {
-        this.http
-          .post(this.post.endPoint, this.post.body(this.contactData))
-          .subscribe({
-            next: (response) => {
-              ngForm.resetForm();
-            },
-            error: (error) => {
-              console.error(error);
-            },
-        complete: () => {
-          this.showSuccess = true;
-          setTimeout(() => {
-            this.showSuccess = false;
-          }, 3000);
-        }
-      });
-      } else if (this.mailTest) {
-        alert('Mail test is active. Form not sent.');
-        ngForm.resetForm();
-      }
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => {
+            this.showSuccess = true;
+            setTimeout(() => {
+              this.showSuccess = false;
+            }, 3000);
+          },
+        });
     }
   }
 
   checkEntries(): boolean {
     this.errorMessage = '';
-
     if (!this.contactData.name) {
       this.errorMessage = this.errorService.getFieldRequiredMessage('Name');
       return false;
     }
-
     if (!this.contactData.email) {
-      this.errorMessage = this.errorService.getFieldRequiredMessage('E-Mail-Adresse');
+      this.errorMessage =
+        this.errorService.getFieldRequiredMessage('E-Mail-Adresse');
       return false;
     }
-
     if (!this.contactData.message) {
-      this.errorMessage = this.errorService.getFieldRequiredMessage('Nachricht');
+      this.errorMessage =
+        this.errorService.getFieldRequiredMessage('Nachricht');
       return false;
     }
-
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(this.contactData.email)) {
       this.errorMessage = this.errorService.getInvalidEmailFormatMessage();
       return false;
     }
-
     return true;
   }
 }
