a78d408c9b973eac8940657307c95154f8a8083b
M1roel <ppfautsch@yahoo.de>
2025-07-22 20:29:18 +0200

    feat(customers): implement customer addition component with form validation

--- src/app/features/customers/customer-add/customer-add.component.ts
+++ src/app/features/customers/customer-add/customer-add.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-customer-add',
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './customer-add.component.html',
   styleUrl: './customer-add.component.scss'
 })
 export class CustomerAddComponent {
+  customerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.customerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      address: ['', Validators.required]
+    });
+  }
 }
