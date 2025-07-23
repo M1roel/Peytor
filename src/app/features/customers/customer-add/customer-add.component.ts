39515cc9179ca23c15fec6412f61d201fd492a46
M1roel <ppfautsch@yahoo.de>
2025-07-23 14:16:34 +0200

    feat(customers): refactor customer add component to use template-driven forms

--- src/app/features/customers/customer-add/customer-add.component.ts
+++ src/app/features/customers/customer-add/customer-add.component.ts
@@ -1,22 +1,56 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'; 
+import { NgForm, FormsModule } from '@angular/forms'; 
+
+interface Address {
+  street: string;
+  house_number: string;
+  zip_code: string;
+  city: string;
+  country: string;
+}
+
+interface Customer {
+  customer_number: string;
+  company_name: string;
+  contact_person: string;
+  vat_id: string;
+  email: string;
+  phone: string;
+  industry: string;
+  website: string;
+  language: string;
+  is_active: boolean;
+  address: Address;
+  notes: string;
+}
 
 @Component({
   selector: 'app-customer-add',
-  imports: [RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './customer-add.component.html',
   styleUrl: './customer-add.component.scss'
 })
 export class CustomerAddComponent {
-  customerForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.customerForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required],
-      address: ['', Validators.required]
-    });
-  }
+  
+  customer: Customer = {
+    customer_number: '',
+    company_name: '',
+    contact_person: '',
+    vat_id: '',
+    email: '',
+    phone: '',
+    industry: '',
+    website: '',
+    language: '',
+    is_active: true,
+    address: {
+      street: '',
+      house_number: '',
+      zip_code: '',
+      city: '',
+      country: ''
+    },
+    notes: ''
+  };
 }
