944617e0a7aaccba608a24d7eef61c73701f9ca9
M1roel <ppfautsch@yahoo.de>
2025-08-26 18:14:00 +0200

    feat(pricing): add router link to contact button for navigation

--- src/app/features/landing/pricing/pricing.component.ts
+++ src/app/features/landing/pricing/pricing.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pricing',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.scss'
 })
