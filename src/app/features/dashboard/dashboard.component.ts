e9f4a60a778018edfd114502ecfa7d34e622bf2b
M1roel <ppfautsch@yahoo.de>
2026-01-10 20:50:15 +0100

    feat(dashboard): enhance dashboard with invoice statistics display and data fetching

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InvoicesService } from '../../core/services/invoices.service';
+import { InvoiceData } from '../../core/models/invoice.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  openInvoices: number = 0;
+  openAmount: number = 0;
 
+  constructor (private invoicesService: InvoicesService) { }
+
+  invoices: InvoiceData[] = [];
+
+  ngOnInit() {
+    this.getInvoices();
+  }
+
+  async getInvoices() {
+    this.invoices = await this.invoicesService.getInvoices();
+    this.openInvoices = this.invoices.filter(invoice => invoice.status !== 'Entwurf').length;
+    this.openAmount = this.invoices
+      .filter(invoice => invoice.status === 'Offen' || invoice.status == 'Überfällig')
+      .reduce((sum, invoice) => sum + invoice.amount, 0);
+  }
 }
